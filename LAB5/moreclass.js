7c0467cce56ab9fbf060a860473ed7b4cce856aa
unknown <prachya.piya@gmail.com>
2020-07-11 19:43:16 +0700

    11/7/2563_2

---
+++ LAB5/moreclass.js
@@ -0,0 +1,42 @@
+//ยกตัวอย่าง ออกแบบสร้าง class คร่าวๆ
+class Car{
+
+    constructor(brand,type,color){
+        this.brand = brand
+        this.type = type
+        this.color= color
+    }
+
+    goForward(){
+
+    }
+
+    goBackward(){
+
+    }
+
+    turnLeft(){
+
+    }
+
+    turnRight(){
+
+    }
+
+    brake(){
+
+    }
+
+    hongAHorn(){
+
+    }
+
+    handBrake(){
+
+    }
+
+    getFuel(){
+
+    }
+
+}
